9d6f3ae9f3fac78b7dbc7a295546911bb2ad6af6
Lotlin <lotlin22@gmail.com>
2024-03-06 17:23:47 +0100

    Update 2024-03-06T16:23:44.454Z

---
+++ js/modules/control.js
@@ -0,0 +1,16 @@
+import {
+  headerMenuBtn, headerNavigation, headerMenuBtnCloseImg, headerMenuBtnOpenImg,
+} from './getElements.js';
+import {toggleElem, switchBurgerBtnImg} from './service.js';
+
+export const mainPageControl = () => {
+  headerMenuBtn.addEventListener('click', () => {
+    toggleElem(headerNavigation, 'navigation--visible');
+    switchBurgerBtnImg(
+        headerMenuBtnCloseImg, 'header__burger-img--show',
+        headerMenuBtnOpenImg, 'header__burger-img--hide',
+    );
+  });
+};
+
+//# sourceMappingURL=../../maps/modules/control.js.map
---
+++ js/modules/getElements.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('.header');
+export const headerMenuBtn = header.querySelector('.header__burger');
+export const headerNavigation = header.querySelector('.navigation--header');
+export const headerMenuBtnOpenImg =
+  header.querySelector('.header__burger-img--open');
+export const headerMenuBtnCloseImg =
+  header.querySelector('.header__burger-img--close');
+
+//# sourceMappingURL=../../maps/modules/getElements.js.map
