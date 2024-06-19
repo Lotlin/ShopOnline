6d4f3dae30d1801c2f4b7c5b447e95eeb33a7eeb
Lotlin <lotlin22@gmail.com>
2024-06-19 10:59:22 +0200

    footer year

--- src/js/modules/API.js
+++ src/js/modules/API.js
@@ -2,5 +2,3 @@ export const SERVER_URL = 'https://fanatical-arrow-empress.glitch.me';
 export const CATEGORIES_URL = `${SERVER_URL}/api/categories`;
 export const GOODS_URL = `${SERVER_URL}/api/goods`;
 export const IMAGES_FOLDER = 'image';
-
-// export const GOODS_API = 'https://github.com/maksim-leskin/cms-backend';
